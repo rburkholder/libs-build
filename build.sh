6a9f54939d30d2904f1f9e4bf39dd8fe77c26246
Raymond Burkholder <raymond@burkholder.net>
2022-03-26 15:40:16 -0600

    build.sh - add a native build for compiler

--- build.sh
+++ build.sh
@@ -626,6 +626,7 @@ function rdaf {
     cmake \
       -Dgnuinstall=ON \
       -imt=ON \
+      -march=native \
       -DCMAKE_CXX_STANDARD=17 \
       -DCMAKE_CXX_STANDARD_REQUIRED=ON \
       -DLLVM_ENABLE_THREADS=ON \
