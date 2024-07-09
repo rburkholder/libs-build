6131a1d74be2aa9afb58955996c533bc040384a6
Raymond Burkholder <raymond@burkholder.net>
2024-07-09 12:47:11 -0600

    build.sh - silent install of files

--- build.sh
+++ build.sh
@@ -617,7 +617,7 @@ function sound {
 }
 
 function repertory {
-  sudo apt install libpaho-mqtt-dev libssl-dev
+  sudo apt -y install libpaho-mqtt-dev libssl-dev
   git clone https://github.com/rburkholder/repertory.git
   pushd repertory
   mkdir build
